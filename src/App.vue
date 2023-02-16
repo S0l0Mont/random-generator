be654e2c0f3bc3c997e4edb6c814d3a8ee2ae4b1
S0l0Mont <castby9weipage@gmail.com>
2023-02-16 23:17:57 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark hide-on-scroll>
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        :src="require('@/assets/logo.png')"
+        transition="scale-transition"
+        width="40"
+      />
+      <v-toolbar-title class="font-weight-black">Spelltacular</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-home</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/CheckDialog.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-dialog v-model="dialog" max-width="290">
+    <v-card>
+      <sweet-alert-icon icon="success"></sweet-alert-icon>
+
+      <v-card-title class="headline">{{ word }}</v-card-title>
+
+      <v-card-text
+        >what is meant by a word, text, concept, or action</v-card-text
+      >
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="primary" text @click="dialog = false"> Alright! </v-btn>
+        <v-spacer></v-spacer>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import SweetAlertIcon from "vue-sweetalert-icons";
+import Vue from "vue";
+Vue.use(SweetAlertIcon);
+export default {
+  name: "CheckDialog",
+  props: {
+    dialog: { type: Boolean },
+    word: { type: String },
+  },
+  data: () => ({
+    //
+  }),
+};
+</script>
